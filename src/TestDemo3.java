bfbdd2700773a97181fec6b4f3cdc929595f79a4
Ganesh Prasad Rai <Ganesh Prasad Rai@LAPTOP-BD1UD23M>
2020-02-04 17:32:36 +0530

    Commiting TestDemo2 to BranchY

---
+++ src/TestDemo3.java
@@ -0,0 +1,12 @@
+
+public class TestDemo3 {
+
+	public TestDemo3() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in GitHub Training.....");
+	}
+}
