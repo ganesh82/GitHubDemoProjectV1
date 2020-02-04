e365249427d4595169a7a16fd629bf3f710abb1f
Ganesh Prasad Rai <Ganesh Prasad Rai@LAPTOP-BD1UD23M>
2020-02-04 16:45:15 +0530

    TestDemo2.java file added

---
+++ src/TestDemo2.java
@@ -0,0 +1,12 @@
+
+public class TestDemo2 {
+
+	public TestDemo2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome in GitHub Training.....");
+	}
+}
